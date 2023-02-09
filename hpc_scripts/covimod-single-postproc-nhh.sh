7a3b482ba8f940d8c3ceef27463968a0de648a5c
Selena Linden <selena.linden19@imperial.ac.uk>
2023-02-09 16:39:56 +0000

    new postproc hpc file for nhh

---
+++ hpc_scripts/covimod-single-postproc-nhh.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#PBS -l walltime=02:00:00
+#PBS -l select=1:ncpus=8:ompthreads=1:mem=50gb
+
+REPO_PATH=/rds/general/user/ssl219/home/bayes-rate-consistency-selena
+WAVE=4
+MODEL="hsgp-eq-cd"
+
+# Post-processing
+MIXING=TRUE
+PPC=TRUE
+PLOT=TRUE
+
+module load anaconda3/personal
+source activate Renv
+
+MODEL=${MODEL}-${WAVE}-"nhh"
+Rscript $REPO_PATH/scripts/postprocess-single.R --model $MODEL --mixing $MIXING --ppc $PPC --plot $PLOT
