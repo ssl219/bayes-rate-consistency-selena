5fb85d036ce45aaa45e3bec89c4680b4978a9f29
Selena Linden <selena.linden19@imperial.ac.uk>
2023-03-20 11:37:37 +0000

    new hh 2 hpc script

--- scripts/run-stan-single-new-hh.R
+++ scripts/run-stan-single-new-hh.R
@@ -14,13 +14,13 @@ option_list <- list(
   optparse::make_option("--seed", type = "integer", default = 0721,
                         help = "Random number seed [default %default]",
                         dest = "seed"),
-  optparse::make_option("--iter_warmup", type = "integer", default = 500,
+  optparse::make_option("--iter_warmup", type = "integer", default = 50,
                         help = "HMC warmup iterations [default %default]",
                         dest = 'iter.warmup'),
-  optparse::make_option("--iter_sampling", type = "integer", default = 1000,
+  optparse::make_option("--iter_sampling", type = "integer", default = 100,
                         help = "HMC of sampling iterations iterations [default %default]",
                         dest = 'iter.sampling'),
-  optparse::make_option("--chains", type = "integer", default = 8,
+  optparse::make_option("--chains", type = "integer", default = 1,
                         help = "Number of MCMC chains",
                         dest = 'chains'),
   optparse::make_option("--model", type = "character", default = "hsgp-eq-cd-new-hh-dropping-all-zeros-symmetric",
@@ -32,23 +32,24 @@ option_list <- list(
   optparse::make_option("--hsgp_m", type = "integer", default = 20,
                         help = "The number of the HSGP basis functions in any dimension [default \"%default\"]",
                         dest = "hsgp_m"),
-  optparse::make_option("--repo_path", type = "character", default = "/rds/general/user/ssl219/home/bayes-rate-consistency-selena",
+  optparse::make_option("--repo_path", type = "character", default = "/Users/mac/Documents/M4R/code/bayes_consistency_rate/bayes-rate-consistency-selena",
                         help = "Absolute file path to repository directory, used as long we don t build an R package [default]",
                         dest = 'repo.path'),
-  optparse::make_option("--data_path", type = "character", default = "/rds/general/user/ssl219/home",
+  optparse::make_option("--data_path", type = "character", default = "/Users/mac/Documents/M4R/code/bayes_consistency_rate",
                         help = "Absolute file path to data directory, used as long we don t build an R package [default]",
                         dest = 'data.path'),
   optparse::make_option("--wave", type = "integer", default = 1,
                         help = "COVIMOD wave",
                         dest = "wave")
 )
-
-# optparse::make_option("--repo_path", type = "character", default = "/Users/mac/Documents/M4R/code/bayes_consistency_rate/bayes-rate-consistency-selena",
+# optparse::make_option("--repo_path", type = "character", default = "/rds/general/user/ssl219/home/bayes-rate-consistency-selena",
 #                       help = "Absolute file path to repository directory, used as long we don t build an R package [default]",
 #                       dest = 'repo.path'),
-# optparse::make_option("--data_path", type = "character", default = "/Users/mac/Documents/M4R/code/bayes_consistency_rate",
+# optparse::make_option("--data_path", type = "character", default = "/rds/general/user/ssl219/home",
 #                       help = "Absolute file path to data directory, used as long we don t build an R package [default]",
 #                       dest = 'data.path'),
+
+
 
 args <- optparse::parse_args(optparse::OptionParser(option_list = option_list))
 
