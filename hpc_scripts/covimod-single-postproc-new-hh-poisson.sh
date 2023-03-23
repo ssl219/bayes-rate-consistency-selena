b4f1ee6a40d1a55b6a6f0b2ae8ced3e4af3ae74b
Selena Linden <selena.linden19@imperial.ac.uk>
2023-03-23 02:31:11 +0000

    postproc hpc new hh poisson

---
+++ hpc_scripts/covimod-single-postproc-new-hh-poisson.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#PBS -l walltime=08:00:00
+#PBS -l select=1:ncpus=8:ompthreads=1:mem=50gb
+
+REPO_PATH=/rds/general/user/ssl219/home/bayes-rate-consistency-selena
+WAVE=1
+MODEL= "hsgp-eq-cd-new-hh-dropping-all-zeros-symmetric-poisson"
+
+# Post-processing
+MIXING=TRUE
+PPC=TRUE
+PLOT=TRUE
+
+module load anaconda3/personal
+source activate Renv
+
+MODEL=${MODEL}-${WAVE}
+Rscript $REPO_PATH/scripts/postprocess-single-new-hh.R --model $MODEL --mixing $MIXING --ppc $PPC --plot $PLOT
