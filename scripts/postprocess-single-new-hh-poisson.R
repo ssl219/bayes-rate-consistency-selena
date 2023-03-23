4fb005685fd591edcad776330ca4c70f55788f5c
Selena Linden <selena.linden19@imperial.ac.uk>
2023-03-23 03:03:26 +0000

    test NA_character_

--- scripts/postprocess-single-new-hh-poisson.R
+++ scripts/postprocess-single-new-hh-poisson.R
@@ -24,7 +24,7 @@ option_list <- list(
   optparse::make_option("--wave", type="integer", default = 1,
                         help = "COVIMOD wave",
                         dest = "wave"),
-  optparse::make_option("--model", type = "character", default = "hsgp-eq-cd-new-hh-dropping-all-zeros-symmetric-poisson-1",
+  optparse::make_option("--model", type = "character", default = NA_character_,
                         help = "Name of the model",
                         dest = "model.name"),
   optparse::make_option("--mixing", type = "logical", default = TRUE,
