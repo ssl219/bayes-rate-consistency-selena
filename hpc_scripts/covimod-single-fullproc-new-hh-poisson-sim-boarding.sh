68e89c1a1b6e888c40a63115b450a58663b22378
Selena Linden <selena.linden19@imperial.ac.uk>
2023-03-27 08:57:44 +0100

    walltime longer

--- hpc_scripts/covimod-single-fullproc-new-hh-poisson-sim-boarding.sh
+++ hpc_scripts/covimod-single-fullproc-new-hh-poisson-sim-boarding.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#PBS -l walltime=48:00:00
+#PBS -l walltime=72:00:00
 #PBS -l select=1:ncpus=10:ompthreads=1:mem=100gb
 
 REPO_PATH=/rds/general/user/ssl219/home/bayes-rate-consistency-selena
