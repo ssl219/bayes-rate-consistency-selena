02e2e8f165e7764984aa6d9c0936498b532d7902
Selena Linden <selena.linden19@imperial.ac.uk>
2023-05-11 17:01:24 +0100

    fullproc check saving time

---
+++ hpc_scripts/covimod-single-fullproc-new-hh-poisson-endsave.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#PBS -l walltime=48:00:00
+#PBS -l select=1:ncpus=10:ompthreads=1:mem=100gb
+
+REPO_PATH=/rds/general/user/ssl219/home/bayes-rate-consistency-selena
+WAVE=1
+MODEL="hsgp-eq-rd-new-hh-dropping-all-zeros-symmetric-poisson"
+HSGP_C=1.5
+HSGP_M=20
+
+# HMC Sampler params
+CHAINS=2
+WARMUP=5
+SAMPLING=10
+
+# Post-processing
+MIXING=TRUE
+
+module load anaconda3/personal
+source activate Renv
+
+Rscript $REPO_PATH/scripts/fullproc-stan-single-new-hh-poisson.R --wave $WAVE --model $MODEL --hsgp_c $HSGP_C --hsgp_m $HSGP_M --chains $CHAINS --iter_warmup $WARMUP --iter_sampling $SAMPLING --mixing $MIXING
