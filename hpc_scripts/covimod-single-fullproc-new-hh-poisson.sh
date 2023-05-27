82fd0939bc03b0b61e1a99da4e486041ba157f1a
Selena Linden <selena.linden19@imperial.ac.uk>
2023-05-27 07:02:56 +0100

    name typo

--- hpc_scripts/covimod-single-fullproc-new-hh-poisson.sh
+++ hpc_scripts/covimod-single-fullproc-new-hh-poisson.sh
@@ -5,7 +5,7 @@
 
 REPO_PATH=/rds/general/user/ssl219/home/bayes-rate-consistency-selena
 WAVE=1
-MODEL="hsgp-eq-rd-new-hh-dropping-all-zeros-symmetric-poisson-multiple-betas"
+MODEL="hsgp-eq-rd-new-hh-dropping-all-zeros-symmetric-poisson"
 HSGP_C=1.5
 HSGP_M=20
 
@@ -20,7 +20,7 @@ MIXING=TRUE
 module load anaconda3/personal
 source activate Renv
 
-Rscript $REPO_PATH/scripts/run-stan-single-new-hh-poisson-multiple-betas.R --wave $WAVE --model $MODEL --hsgp_c $HSGP_C --hsgp_m $HSGP_M --chains $CHAINS --iter_warmup $WARMUP --iter_sampling $SAMPLING
+Rscript $REPO_PATH/scripts/run-stan-single-new-hh-poisson.R --wave $WAVE --model $MODEL --hsgp_c $HSGP_C --hsgp_m $HSGP_M --chains $CHAINS --iter_warmup $WARMUP --iter_sampling $SAMPLING
 
 MODEL=${MODEL}-${WAVE}
-Rscript $REPO_PATH/scripts/postprocess-single-new-hh-poisson-multiple-betas.R --model $MODEL --mixing $MIXING
+Rscript $REPO_PATH/scripts/postprocess-single-new-hh-poisson.R --model $MODEL --mixing $MIXING
