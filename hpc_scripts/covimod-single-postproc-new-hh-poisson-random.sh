5270619864aa56c179d03916270f328198acf29c
Selena Linden <selena.linden19@imperial.ac.uk>
2023-05-27 07:08:07 +0100

    random

---
+++ hpc_scripts/covimod-single-postproc-new-hh-poisson-random.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#PBS -l walltime=08:00:00
+#PBS -l select=1:ncpus=8:ompthreads=1:mem=100gb
+
+REPO_PATH=/rds/general/user/ssl219/home/bayes-rate-consistency-selena
+WAVE=1
+MODEL="hsgp-eq-rd-new-hh-dropping-all-zeros-symmetric-poisson-1-random"
+
+# Post-processing
+MIXING=TRUE
+PPC=TRUE
+PLOT=TRUE
+
+module load anaconda3/personal
+source activate Renv
+
+Rscript $REPO_PATH/scripts/postprocess-single-new-hh-poisson-random.R --model $MODEL --mixing $MIXING --ppc $PPC --plot $PLOT
