de004ce41f38f880a1a20cb2852c4080e950a70c
Selena Linden <selena.linden19@imperial.ac.uk>
2023-06-02 07:06:06 +0100

    postproc nhh other

--- hpc_scripts/covimod-single-postproc-nhh.sh
+++ hpc_scripts/covimod-single-postproc-nhh.sh
@@ -8,12 +8,12 @@ WAVE=1
 MODEL="hsgp-eq-rd"
 
 # Post-processing
-MIXING=FALSE
+MIXING=TRUE
 PPC=TRUE
 PLOT=TRUE
 
 module load anaconda3/personal
 source activate Renv
 
-MODEL=${MODEL}-${WAVE}-"nhh"
+MODEL=${MODEL}-${WAVE}-"nhh-other"
 Rscript $REPO_PATH/scripts/postprocess-single-nhh.R --model $MODEL --mixing $MIXING --ppc $PPC --plot $PLOT
