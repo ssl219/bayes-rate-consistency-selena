c8060d947b018026d6ea3e776612af05987643ee
Selena Linden <selena.linden19@imperial.ac.uk>
2023-06-04 17:59:58 +0100

    500 boarding

--- hpc_scripts/simulation-fullproc-new-hh.sh
+++ hpc_scripts/simulation-fullproc-new-hh.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #PBS -l walltime=8:00:00
-#PBS -l select=1:ncpus=8:ompthreads=1:mem=50gb
+#PBS -l select=1:ncpus=8:ompthreads=1:mem=100gb
 
 REPO_PATH=/rds/general/user/ssl219/home/bayes-rate-consistency-selena
 DATA_PATH=/rds/general/user/ssl219/home
@@ -9,7 +9,7 @@ WAVE=1
 MODEL="hsgp-eq-rd-new-hh-dropping-all-zeros-symmetric-poisson"
 HSGP_C=1.5
 HSGP_M=20
-N=250
+N=500
 HHSIZE=4
 SCENARIO="boarding_school"
 SIM_NO=1
