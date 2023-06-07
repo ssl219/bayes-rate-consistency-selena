05d91b194f8f557f6ebba07861f145f7219f8311
Selena Linden <selena.linden19@imperial.ac.uk>
2023-06-07 06:32:47 +0100

    more mem

--- hpc_scripts/covimod-single-postproc-new-hh-poisson.sh
+++ hpc_scripts/covimod-single-postproc-new-hh-poisson.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #PBS -l walltime=8:00:00
-#PBS -l select=1:ncpus=8:ompthreads=1:mem=100gb
+#PBS -l select=1:ncpus=8:ompthreads=1:mem=500gb
 
 REPO_PATH=/rds/general/user/ssl219/home/bayes-rate-consistency-selena
 WAVE=1
